0c73c57092c05708ff69b7f7ddbd930df2a7b80b
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-01-02 16:03:00 +0530

    REFACTORED: Remove UnUse Print Statement and Main Method

--- src/main/java/com/bridgelabz/MaximumNumber.java
+++ src/main/java/com/bridgelabz/MaximumNumber.java
@@ -12,19 +12,6 @@ public class MaximumNumber {
         if (z.compareTo(max) > 0)
             max = z;
 
-        System.out.println(x+" "+y+" "+z);
-
-        System.out.println("Max No. is :"+max);
-
         return max;
-    }
-
-    public static void main(String[] args) {
-
-        int x = 10;
-        int y = 40;
-        int z = 30;
-
-        findMaxNo(x,y,z);
     }
 }
