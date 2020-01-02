abf276a4db5f81037774d686955053d7706c9c28
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-01-02 16:33:24 +0530

    ADDED: Test Cases For Find Max No. Of Float Type Of 3 Numbers

--- src/main/java/com/bridgelabz/MaximumNumber.java
+++ src/main/java/com/bridgelabz/MaximumNumber.java
@@ -14,4 +14,17 @@ public class MaximumNumber {
 
         return max;
     }
+
+    public static Float findMaxNoForFloat(Float x, Float y, Float z){
+
+        Float max = x;
+
+        if (y.compareTo(max) > 0)
+            max = y;
+
+        if (z.compareTo(max) > 0)
+            max = z;
+
+        return max;
+    }
 }
--- src/test/java/TestMaximumNumber.java
+++ src/test/java/TestMaximumNumber.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 public class TestMaximumNumber {
 
+    // Test Cases For Integer Number
     @Test
     public void whenGivenFirstNumber_IfItsMax_ShouldReturnSameNo() {
 
@@ -26,5 +27,31 @@ public class TestMaximumNumber {
         MaximumNumber maximumNumber = new MaximumNumber();
         int maxNo = maximumNumber.findMaxNo(400, 200, 500);
         Assert.assertEquals(500, maxNo);
+    }
+
+
+    // Test Cases For Float Number
+    @Test
+    public void whenGivenFirstFloatNumber_IfItsMax_ShouldReturnSameNo() {
+
+        MaximumNumber maximumNumber = new MaximumNumber();
+        float maxNo = maximumNumber.findMaxNoForFloat(400.0f, 200.0f, 100.0f);
+        Assert.assertEquals(400.0, maxNo,0.0);
+    }
+
+    @Test
+    public void whenGivenSecondFloatNumber_IfItsMax_ShouldReturnSameNo() {
+
+        MaximumNumber maximumNumber = new MaximumNumber();
+        float maxNo = maximumNumber.findMaxNoForFloat(100.0f, 300.0f, 150.0f);
+        Assert.assertEquals(300.0, maxNo,0.0);
+    }
+
+    @Test
+    public void whenGivenThirdFloatNumber_IfItsMax_ShouldReturnSameNo() {
+
+        MaximumNumber maximumNumber = new MaximumNumber();
+        float maxNo = maximumNumber.findMaxNoForFloat(400.0f, 200.0f, 500.0f);
+        Assert.assertEquals(500.0, maxNo,0.0);
     }
 }
